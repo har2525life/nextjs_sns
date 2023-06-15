965d9639e7a9fc8bdb9b95489a7485b605ef00bb
har2525life <har2525life@outlook.com>
2023-06-15 16:36:26 +0900

    add: cors setting

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express'
 import authRoute from "./routes/auth"
-
+import cors from "cors"
 const app: Application = express()
 
 // localのport番号設定
@@ -8,6 +8,7 @@ const PORT = 5000
 
 // json形式でdataが送られてくることを明記
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/auth", authRoute)
 
