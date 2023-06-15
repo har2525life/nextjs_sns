e9e3e8fdb4c6d58cabfcd3d17439c3ad7b4ba2ad
har2525life <har2525life@outlook.com>
2023-06-15 10:10:12 +0900

    add: localhost:5000 Hello display

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,13 @@
-import express, { Application } from "express"
+import express, { Application, Request, Response } from "express"
 
 const app: Application = express()
+
+// localのport番号設定
 const PORT = 5000
+
+// API作成
+app.get("/", (req: Request, res: Response) => {
+    res.send("<h1>Hello</h1>")
+})
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`))
