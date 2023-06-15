caf0dcaba2ed535c6e313dd6089243f1818a703e
har2525life <har2525life@outlook.com>
2023-06-15 11:18:15 +0900

    fix: password to hash value, request data json

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from "bcrypt"
 
 const app: Application = express()
 const prisma = new PrismaClient()
 
 // localのport番号設定
 const PORT = 5000
+
+// json形式でdataが送られてくることを明記
+app.use(express.json())
 
 // API作成
 // app.get("/", (req: Request, res: Response) => {
@@ -15,13 +19,13 @@ const PORT = 5000
 // 新規ユーザー登録API
 app.post('/api/auth/register', async (req: Request, res: Response) => {
     const { username, email, password }: { username: string; email: string; password: string } = req.body
-
+    const hash = bcrypt.hashSync(password, 10); // hash value, salt rounds
     // prisma.userのuserはDBのtable名を小文字にしたもの
     const user = await prisma.user.create({
         data: {
             username,
             email,
-            password
+            password: hash
         }
     })
 
