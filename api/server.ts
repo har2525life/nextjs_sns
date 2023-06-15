47ae6e5734f84f43b723f484edcea4322f07e067
har2525life <har2525life@outlook.com>
2023-06-15 10:53:24 +0900

    add: create user api

--- api/server.ts
+++ api/server.ts
@@ -1,13 +1,31 @@
-import express, { Application, Request, Response } from "express"
+import express, { Application, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app: Application = express()
+const prisma = new PrismaClient()
 
 // localのport番号設定
 const PORT = 5000
 
 // API作成
-app.get("/", (req: Request, res: Response) => {
-    res.send("<h1>Hello</h1>")
+// app.get("/", (req: Request, res: Response) => {
+//     res.send("<h1>Hello</h1>")
+// })
+
+// 新規ユーザー登録API
+app.post('/api/auth/register', async (req: Request, res: Response) => {
+    const { username, email, password }: { username: string; email: string; password: string } = req.body
+
+    // prisma.userのuserはDBのtable名を小文字にしたもの
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password
+        }
+    })
+
+    return res.json({ user })
 })
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`))
