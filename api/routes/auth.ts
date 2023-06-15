f47a38ed9f1ae479d48f3fd1c2a9da8bdd76c8ba
har2525life <har2525life@outlook.com>
2023-06-15 13:37:45 +0900

    fix : route auth, file split

---
+++ api/routes/auth.ts
@@ -0,0 +1,56 @@
+import { Router, Request, Response} from "express"
+import bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client"
+import { sign } from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+const prisma = new PrismaClient()
+
+const router = Router()
+
+// 新規ユーザー登録API
+router.post('/register', async (req: Request, res: Response) => {
+    const { username, email, password }: { username: string; email: string; password: string } = req.body
+    const hash = bcrypt.hashSync(password, 10) // hash value, salt rounds
+    // prisma.userのuserはDBのtable名を小文字にしたもの
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hash
+        }
+    })
+
+    return res.json({ user })
+})
+
+// ユーザーログインAPI
+router.post('/login', async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    // findMany　検索する時に使用 返り値が配列になる
+    // whereでどのカラムか
+    // findUnique 一意の値が返り値となる
+    const user = await prisma.user.findUnique({ where: { email } })
+
+    if (!user) {
+        return res.status(401).json({ error: 'email or passwordが違います。' })
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ error: 'パスワードが' })
+    }
+
+    // tokenにする値,　シークレットキー
+    const token = sign({ id: user.id }, process.env.SECRET_KEY!, {
+        expiresIn: '1d'
+    })
+
+    return res.json({ token })
+})
+
+export default router
--- api/server.ts
+++ api/server.ts
@@ -1,12 +1,7 @@
-import express, { Application, Request, Response } from 'express'
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcrypt'
-import { sign } from 'jsonwebtoken'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import express, { Application } from 'express'
+import authRoute from "./routes/auth"
 
 const app: Application = express()
-const prisma = new PrismaClient()
 
 // localのport番号設定
 const PORT = 5000
@@ -14,52 +9,6 @@ const PORT = 5000
 // json形式でdataが送られてくることを明記
 app.use(express.json())
 
-// API作成
-// app.get("/", (req: Request, res: Response) => {
-//     res.send("<h1>Hello</h1>")
-// })
-
-// 新規ユーザー登録API
-app.post('/api/auth/register', async (req: Request, res: Response) => {
-    const { username, email, password }: { username: string; email: string; password: string } = req.body
-    const hash = bcrypt.hashSync(password, 10) // hash value, salt rounds
-    // prisma.userのuserはDBのtable名を小文字にしたもの
-    const user = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hash
-        }
-    })
-
-    return res.json({ user })
-})
-
-// ユーザーログインAPI
-app.post('/api/auth/login', async (req: Request, res: Response) => {
-    const { email, password } = req.body
-
-    // findMany　検索する時に使用 返り値が配列になる
-    // whereでどのカラムか
-    // findUnique 一意の値が返り値となる
-    const user = await prisma.user.findUnique({ where: { email } })
-
-    if (!user) {
-        return res.status(401).json({ error: 'email or passwordが違います。' })
-    }
-
-    const isPasswordValid = bcrypt.compareSync(password, user.password)
-
-    if (!isPasswordValid) {
-        return res.status(401).json({ error: 'パスワードが' })
-    }
-
-    // tokenにする値,　シークレットキー
-    const token = sign({ id: user.id }, process.env.SECRET_KEY!, {
-        expiresIn: '1d'
-    })
-
-    return res.json({ token })
-})
+app.use("/api/auth", authRoute)
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`))
