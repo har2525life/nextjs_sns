982d07bb17f77c471688db85d58d66a7e5a14555
har2525life <har2525life@outlook.com>
2023-06-19 14:05:38 +0900

    add: display content types

--- client/types/auth.d.ts
+++ client/types/auth.d.ts
@@ -1,10 +1,26 @@
 type Login = {
-    email: string;
-    password: string;
+    email: string
+    password: string
 }
 
 type Signup = {
-    username: string;
-    email: string;
-    password: string;
+    username: string
+    email: string
+    password: string
+}
+
+type User = {
+    id: string
+    username: string
+    email: string
+    password: string
+    posts: Post[]
+}
+
+type Post = {
+    id: number
+    content: string
+    createdAt: string
+    authId: string
+    auth: User
 }
